7fcc46a59676ebb20220c480b3aa14f6e992f352
daelvinci <elvinab@code.edu.az>
2023-01-12 12:54:28 +0400

    added

---
+++ 1-12-2022/Pratice delegate/Pratice delegate/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Pratice_delegate
+{
+    internal class Program
+    {
+        delegate int Calculate(int num1, int num2);
+        delegate bool CheckNum(int num);
+
+        static int Sum(int[] numbers, Predicate<int> check)
+        {
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                if (check(num))
+                    sum += num;
+            }
+
+            return sum;
+        }
+        static int SumOfOdd(int[] numbers)
+        {
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 1)
+                    sum += num;
+            }
+
+            return sum;
+        }
+
+        static int SumOfDividedBy3(int[] numbers)
+        {
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                if (num % 3 == 0)
+                    sum += num;
+            }
+
+            return sum;
+        }
+
+        static int SumOfDividedBy5(int[] numbers)
+        {
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                if (num % 5 == 0)
+                    sum += num;
+            }
+
+            return sum;
+        }
+
+        static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        static bool IsOdd(int num)
+        {
+            return num % 2 == 1;
+        }
+
+        static void ShowSum(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 45, 10, 90, 83, 24, 54, 23, 66 };
+
+            CheckNum method = IsEven;
+            method = IsOdd;
+            method = delegate (int num) {
+                return num % 3 == 0;
+            };
+            method = x => x % 5 == 0;
+
+            var result = method.Invoke(34);
+            result = method(10);
+
+            Console.WriteLine(Sum(numbers, IsEven));
+            Console.WriteLine(Sum(numbers, IsOdd));
+            Console.WriteLine(Sum(numbers, delegate (int num) { return num < 0; }));
+            Console.WriteLine(Sum(numbers, x => x > 0));
+            Console.WriteLine(Sum(numbers, x => x < 0 && x % 3 == 0));
+
+
+            Console.WriteLine(SumOfDividedBy3(numbers));
+            Console.WriteLine(SumOfDividedBy5(numbers));
+            Console.WriteLine(SumOfOdd(numbers));
+
+            Calculate add = delegate (int x, int y) { return x + y; };
+
+            add = (x, y) => x * y;
+            Console.WriteLine(add(10, 30));
+
+
+            Func<int, bool> check = IsEven;
+            Func<int, int, int> sum = (x, y) => x + y;
+
+            Action<int, int> sum2 = ShowSum;
+
+            Predicate<int> predicate = IsEven;
+            predicate = x => x % 3 == 0;
+            predicate = delegate (int x) { return x > 100; };
+
+        }
+
+    }
+}
---
+++ 1-12-2022/Pratice delegate/zumkuler/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace zumkuler
+{
+    internal class Program
+    {
+        delegate bool Num(int num);
+        delegate bool Nums(int num1, int num2);
+        delegate string Parse(int numP);
+
+        public static string ParseToStr(int numP)
+        {
+            return numP.ToString();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Num metod1 = delegate (int reqem)
+            {
+                return 7 * reqem == 28;
+
+            };
+            Nums metod2 = delegate (int a, int b)
+            {
+                return a % 2 == 0 && b + a == 2;
+            };
+            Parse parseMetod = ParseToStr;
+
+            parseMetod = delegate (int ijn)
+            {
+                return ijn.ToString();
+            };
+
+            metod1 = x => x %3==1; 
+
+            var resultMetod1=metod1.Invoke(34);
+            Console.WriteLine(resultMetod1);
+
+            var resultMetod2= metod2(23, 56);
+            Console.WriteLine(resultMetod2);
+            Console.WriteLine(SumIsPossible(23,2,metod2));
+            Console.WriteLine(SumIsPossible(12,23,delegate (int num, int num2) { return num / num2==0; }));
+            Console.WriteLine(SumIsPossible(2, 2, (x,a) => x == 3 && a+x==2));
+
+            metod1(2);
+
+
+            Func<int, bool> check = metod1.Invoke;
+            check(2);
+
+            Func<int, int, int> sum = (x, y) => x + y;
+
+            Action<int, int> sum2 = ShowSum(3, 3);
+           
+
+            //Predicate<int> predicate = IsEven;
+            //predicate = x => x % 3 == 0;
+            //predicate = delegate (int x) { return x > 100; };
+        }
+        static bool SumIsPossible(int nm, int nm2, Nums nums )
+        {
+            return nums(nm, nm2);
+        }
+        static void ShowSum(int num, int num2)
+        {
+            Console.WriteLine( num / num2);
+        }
+        
+    }
+}
